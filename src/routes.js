36f2d5918721ea09fd598f55c71225a7578254e0
CORP\ldelgros <luca.del-grosso@capgemini.com>
2025-11-07 18:27:15 +0100

    .

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,10 @@
-export const routes = [
-  {
-    path: '/home',
-    component: 'home-view',
-    action: () => import('./components/home-view.js'),
-  },
-  {
-    path: '/favorited',
-    component: 'favorited-view',
-    action: () => import('./components/favorited-view.js'),
-  },
-  { path: '', redirect: '/home' },
+const router = new Router(outlet, {
+  baseUrl: '/Real-Estate-Lint-Element-3-FrontEnd/'
+});
+
+router.setRoutes([
+  { path: '/home', component: 'home-view', action: () => import('./components/home-view.js') },
+  { path: '/favorited', component: 'favorited-view', action: () => import('./components/favorited-view.js') },
   { path: '/', redirect: '/home' },
   { path: '(.*)', redirect: '/home' }
-]   
+]);   
