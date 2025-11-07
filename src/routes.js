7a947b60cacb6065c998fe9930e268a2105bcf45
CORP\ldelgros <luca.del-grosso@capgemini.com>
2025-11-07 18:16:19 +0100

    adding routing for production

--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,6 @@ export const routes = [
     action: () => import('./components/favorited-view.js'),
   },
   { path: '', redirect: '/home' },
-  { path: '/', redirect: '/home' }
+  { path: '/', redirect: '/home' },
+  { path: '/Real-Estate-Lint-Element-3-FrontEnd/', redirect: '/home' }
 ]   
